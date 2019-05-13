244ca2eadfd7152ee5abb7287f2bbf79ea6619da
wipegup <wipegup@gmail.com>
2019-05-13 09:54:54 -0600

    Returning all stations

--- app/facade/search_info_facade.rb
+++ app/facade/search_info_facade.rb
@@ -1,6 +1,17 @@
 class SearchInfoFacade
   def initialize(zip_code)
-
+    @zip_code = zip_code
+    binding.pry
   end
 
+
+  private
+
+  def search_service
+    @service ||= SearchService.new(@zip_code)
+  end
+
+  def all_stations
+    @stations ||= search_service.all_stations
+  end
 end
