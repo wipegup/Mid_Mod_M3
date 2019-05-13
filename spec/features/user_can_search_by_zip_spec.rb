69b9ca070c9fd604a21cf13da9e934534c11dcf5
wipegup <wipegup@gmail.com>
2019-05-13 09:31:55 -0600

    Added search controller, starting to work on calling to api

--- spec/features/user_can_search_by_zip_spec.rb
+++ spec/features/user_can_search_by_zip_spec.rb
@@ -3,9 +3,11 @@ require 'rails_helper'
 feature "User can visit root page" do
   scenario "and view nearest stations by zip" do
     visit "/"
+    save_and_open_page
 
-    fill_in "AltFuelFinder", with: 80206
-    click_button "Search"
+    fill_in "q", with: 80206
+
+    click_button "Locate"
 
     expect(current_path).to eq(search_path)
     expect(page).to have_content("93 Stations")
